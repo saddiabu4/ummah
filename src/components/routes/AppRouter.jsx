ae387d2ae751aaf4049aefe9cab4be3e7d92d981
bakhriddinovvv <baxriddinov.dv@gmail.com>
2025-05-29 22:01:55 +0500

    add not found page

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -4,6 +4,7 @@ import Home from '../Home';
 import SignInPage from '../pages/auth/SignInPage';
 import SignUpPage from '../pages/auth/SignUpPage';
 import DashboardLayoutBasic from '../pages/admin/DashboardLayoutBasic';
+import NotFound from '../pages/NotFound';
 const AppRouter = () => {
   return (
     <Routes>
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path='/signin' element={<SignInPage />} />
       <Route path='/signup' element={<SignUpPage />} />
       <Route path='/admin' element={<DashboardLayoutBasic />} />
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   );
 };
