6e91a3a8fd1a1b24a16ce35abb194d2d2d9023dc
Abdulatif <saddiabu4@gmail.com>
2025-05-29 17:33:37 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-amber-700 font-bold text-center'>Welcome to My App</h1>
+      <p className='text-amber-700 text-center'>This is a paragraph.</p>
+      
+    </>
+  )
+}
+
+export default App
