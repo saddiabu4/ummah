262053a0574a5f4cd1e56c1eeb4bf8bc1958c86d
bakhriddinovvv <baxriddinov.dv@gmail.com>
2025-05-29 14:09:58 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { Toaster } from 'sonner'
 function App() {
 
   return (
     <>
       <h1>Welcome to My App</h1>
+      
+      {/* Toasters */}
+      <Toaster richColors position="top-right" />
     </>
   )
 }
